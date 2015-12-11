2f43c9988e9ba3d6f75bd90e00ff5e07794e6336
jason2014-liu <379626076@qq.com>
2015-12-11 10:25:57 +0800

    update

--- lmframework/src/test/java/test/framework/bss/service/ModuleServiceTest.java
+++ lmframework/src/test/java/test/framework/bss/service/ModuleServiceTest.java
@@ -10,17 +10,12 @@
 */
 package test.framework.bss.service;
 
-import java.util.List;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import com.lmstudio.framework.bss.bo.Module;
-import com.lmstudio.framework.bss.service.impl.ModuleService;
 
 /**
 * TODO
@@ -34,19 +29,10 @@ public class ModuleServiceTest {
 	@Autowired
 	private ApplicationContext applicationContext;
 	
-	@Autowired
-	private ModuleService moduleService;
 	
 	@Test
 	public void testFindModulesByUser(){
-		System.out.println("-------------------------------------------------");
-		List<Module> list = moduleService.findModulesByUser("100");
-		if(list == null){
-			System.out.println("00000000000");
-		}
-		System.out.println(list);
+	
 	}
-	
-	
 
 }
